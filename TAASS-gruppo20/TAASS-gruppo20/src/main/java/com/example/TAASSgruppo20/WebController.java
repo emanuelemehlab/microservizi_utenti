335bbffd06ed946b2c2bf418b4ff12bc4861ffa5
pie.vit <pietro.vitale23@gmail.com>
2022-11-29 13:13:06 +0100

    Creazione pagina login in html

--- TAASS-gruppo20/TAASS-gruppo20/src/main/java/com/example/TAASSgruppo20/WebController.java
+++ TAASS-gruppo20/TAASS-gruppo20/src/main/java/com/example/TAASSgruppo20/WebController.java
@@ -1,14 +1,16 @@
 package com.example.TAASSgruppo20;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class WebController {
 
     @GetMapping("/")
-    public String login(){
-        return "Login :)";
+    public ModelAndView login(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("login.html");
+        return mv;
     }
 }
