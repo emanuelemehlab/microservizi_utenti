e17b9390e8a1a054229d9a279caa76f32cd4e089
emanuelemehlab <61579440+emanuelemehlab@users.noreply.github.com>
2022-11-29 12:32:23 +0100

    Inizio progetto

---
+++ TAASS-gruppo20/TAASS-gruppo20/src/main/java/com/example/TAASSgruppo20/TaassGruppo20Application.java
@@ -0,0 +1,13 @@
+package com.example.TAASSgruppo20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaassGruppo20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaassGruppo20Application.class, args);
+	}
+
+}
---
+++ TAASS-gruppo20/TAASS-gruppo20/src/test/java/com/example/TAASSgruppo20/TaassGruppo20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TAASSgruppo20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaassGruppo20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
