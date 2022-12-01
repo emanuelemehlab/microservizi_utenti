507d7ec9bcf9415508036cbd5df9553fb8939504
SavNin <saverio.ninivaggi@gmail.com>
2022-12-01 19:57:00 +0100

    Prima gestione del login e token oauth2. Da aggiustare l'inserimento nel db modificando l'entità cliente

--- microservizi_utenti/src/main/java/com/example/microservizi_utenti/Configuration/configurationOauth.java
+++ microservizi_utenti/src/main/java/com/example/microservizi_utenti/Configuration/configurationOauth.java
@@ -3,17 +3,23 @@ package com.example.microservizi_utenti.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class configurationOauth {
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            http.authorizeRequests()
+            http.authorizeHttpRequests()
                     .anyRequest().authenticated()
                     .and()
                     .oauth2Login();
             return http.build();
+        }
+
+        @Bean
+        public WebSecurityCustomizer webSecurityCustomizer() {
+            return (web) -> web.ignoring().requestMatchers("/");
         }
     }
 
