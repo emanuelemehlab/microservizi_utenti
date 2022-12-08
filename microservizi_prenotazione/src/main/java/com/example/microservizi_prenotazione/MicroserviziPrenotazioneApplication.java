9c7e217a4b1998a904fca53c5ef513f716667462
emanuelemehlab <61579440+emanuelemehlab@users.noreply.github.com>
2022-12-08 17:05:20 +0100

    Aggiunta progetto microservizio prenotazione

---
+++ microservizi_prenotazione/src/main/java/com/example/microservizi_prenotazione/MicroserviziPrenotazioneApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservizi_prenotazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviziPrenotazioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviziPrenotazioneApplication.class, args);
+	}
+
+}
