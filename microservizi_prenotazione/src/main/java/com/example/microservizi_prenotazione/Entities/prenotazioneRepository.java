b025f77394ad0785dc5f3d811c1232a0fbe99022
pie.vit <pietro.vitale@gmail.com>
2022-12-19 20:24:35 +0100

    Aggiunte funzioni POST per la gestione delle prenotazioni + debugging. Da testare il tutto.

--- microservizi_prenotazione/src/main/java/com/example/microservizi_prenotazione/Entities/prenotazioneRepository.java
+++ microservizi_prenotazione/src/main/java/com/example/microservizi_prenotazione/Entities/prenotazioneRepository.java
@@ -2,5 +2,5 @@ package com.example.microservizi_prenotazione.Entities;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ricercarepository extends JpaRepository<ricercaprenotazione, Long> {
+public interface prenotazioneRepository extends JpaRepository<prenotazione, Long> {
 }
--- microservizi_utenti/src/main/java/com/example/microservizi_utenti/Entities/TassistaRepository.java
+++ microservizi_utenti/src/main/java/com/example/microservizi_utenti/Entities/TassistaRepository.java
@@ -10,4 +10,7 @@ public interface TassistaRepository extends JpaRepository<Tassista, String> {
     @Query("SELECT tas.email,tas.nome,tas.cognome,d.disponibilita,a.targa,a.descrizione,a.nPosti,a.bagagliaio,a.seggiolino FROM Tassista AS tas, Disponibilita AS d, Automobile AS a  WHERE  tas.email=d.email AND d.email=a.tassista.email AND tas.email = ?1")
     public List<Object> findByEmail(String email);
 
+    @Query("SELECT tas.email,tas.nome,tas.cognome,d.disponibilita,a.targa,a.descrizione,a.nPosti,a.bagagliaio,a.seggiolino FROM Tassista AS tas, Disponibilita AS d, Automobile AS a  WHERE  tas.email=d.email AND d.email=a.tassista.email AND tas.email IN ?1")
+    public List<Object> findAllInfo(List<String> tassisti);
+
 }
