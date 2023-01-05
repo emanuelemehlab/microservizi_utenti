d7d16f87633deebd266b25565f18b1782daebe16
emanuelemehlab <61579440+emanuelemehlab@users.noreply.github.com>
2023-01-05 09:50:22 +0100

    Delete microservizi_prenotazione directory

--- microservizi_prenotazione/src/main/java/com/example/microservizi_prenotazione/Entities/prenotazioneRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.microservizi_prenotazione.Entities;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface prenotazioneRepository extends JpaRepository<prenotazione, Long> {
-}
